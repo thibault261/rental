c57872dde6188d0b73ed6722745a8170e6cdb532
Martin <thibault.martin30@gmail.com>
2017-07-08 09:13:16 +0200

    Init application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rental::Application.config.secret_key_base = '0f4e12c541e6d1abe778dcc6c92f27df2143be9f1b97daf900468955e5affd73c67764ae9c07278670db64134c63fc75569087538d2ca68d4a097356684c07bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rental::Application.config.session_store :cookie_store, key: '_rental_session'
